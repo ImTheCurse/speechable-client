9fb5e72d019c3b7ffbd275ce1d158c041f574e97
Rani Giro <raniking15@gmail.com>
2024-10-06 15:18:11 +0300

    Structure: Added search button icon.

--- src/comp/layout/layout.tsx
+++ src/comp/layout/layout.tsx
@@ -4,17 +4,28 @@ import AvatarItem from "./avatar";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 import { IconButton } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
 
 export default function Layout(prop: { themeToggle: Function }) {
   const theme = useContext(ThemeContext);
   const textColor = theme === "dark" ? "text-white" : "text-black";
+
+  const lightModeIconStyle = {
+    color: "black",
+    marginRight: "1.5rem",
+  };
+
+  const darkModeIconStyle = {
+    color: "white",
+    marginRight: "1.5rem",
+  };
 
   const themeIcon =
     theme === "dark" ? (
       <IconButton
         onClick={prop.themeToggle}
         aria-label="global-theme"
-        sx={{ color: "white", marginRight: "2rem" }}
+        sx={darkModeIconStyle}
       >
         <DarkModeOutlinedIcon />
       </IconButton>
@@ -22,9 +33,20 @@ export default function Layout(prop: { themeToggle: Function }) {
       <IconButton
         onClick={prop.themeToggle}
         aria-label="theme"
-        sx={{ color: "black", marginRight: "2rem" }}
+        sx={lightModeIconStyle}
       >
         <WbSunnyOutlinedIcon />
+      </IconButton>
+    );
+
+  const searchIcon =
+    theme === "dark" ? (
+      <IconButton sx={darkModeIconStyle}>
+        <SearchOutlined />
+      </IconButton>
+    ) : (
+      <IconButton sx={lightModeIconStyle}>
+        <SearchOutlined />
       </IconButton>
     );
 
@@ -59,6 +81,7 @@ export default function Layout(prop: { themeToggle: Function }) {
         </li>
       </ul>
       <div className="flex justify-end items-center">
+        {searchIcon}
         {themeIcon}
         <AvatarItem />
       </div>
